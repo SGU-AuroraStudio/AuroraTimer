2366d49b4ec72fc3ffe1ce166ca1f2963077df47
18125061059-YaoRenzhao <40916709+JhonTitor6@users.noreply.github.com>
2020-11-03 21:34:57 +0800

    一些小改动
    表格取消选中

--- src/aurora/timer/client/view/WorkForm.java
+++ src/aurora/timer/client/view/WorkForm.java
@@ -5,6 +5,12 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Date;
+import java.time.DayOfWeek;
 
 public class WorkForm {
     public static JFrame FRAME;
@@ -17,7 +23,17 @@ public class WorkForm {
     private static final int MAX_CHARACTERS = 300;
     public WorkForm(){
         parent.setOpaque(false);
-
+        parent.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println(x+" "+y);
+                if (dutyList.isEditing())
+                    dutyList.getCellEditor().stopCellEditing();
+                dutyList.clearSelection();
+            }
+        });
         // 设置表头，和数据
         Object[] columnNames = {"星期天","星期一","星期二","星期三","星期四","星期五","星期六"};
         Object[][] data = {{"张三","张三","张三","张三","张三","张三","张三"}};
@@ -26,6 +42,20 @@ public class WorkForm {
         // 公告栏输入框
         announceText.setFont(new Font("YaHei Consolas Hybrid", Font.PLAIN, 18));
         announceText.setLineWrap(true);
+        announceText.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (dutyList.isEditing())
+                    dutyList.getCellEditor().stopCellEditing();
+                dutyList.clearSelection();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
         announceButton.setUI(new LoginButtonUI());
         // 公告栏外面的框架
         jspAnnounce.setViewportBorder(null);
@@ -37,6 +67,7 @@ public class WorkForm {
         dutyList.setBorder(null);
         dutyList.setFont(new Font("YaHei Consolas Hybrid", Font.PLAIN, 16));
         dutyList.setBackground(new Color(0, 0, 0, 0));
+        dutyList.setSelectionBackground(new Color(86, 209, 149));
         DefaultTableCellRenderer defaultTableCellRenderer = (DefaultTableCellRenderer) dutyList.getDefaultRenderer(Object.class);
         defaultTableCellRenderer.setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
         //TODO:点击表格外的位置取消选中
@@ -65,6 +96,8 @@ public class WorkForm {
         FRAME.setSize(565,400);
         FRAME.setBounds((d.width - FRAME.getWidth()) / 2, (d.height - FRAME.getHeight()) / 2, FRAME.getWidth(), FRAME.getHeight());
         FRAME.setVisible(true);
+        FRAME.setAlwaysOnTop(true);
+        FRAME.setAlwaysOnTop(false);
     }
 }
 
